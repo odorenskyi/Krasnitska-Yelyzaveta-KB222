48988a76841c9b868c01195c400663381ae7e7f9
Lisa Krasnitska <114089485+lissavzx@users.noreply.github.com>
2023-04-23 19:05:19 +0300

    Update ModulesKrasnitska.h

--- lab08/prj/ModulesKrasnitska.h
+++ lab08/prj/ModulesKrasnitska.h
@@ -4,5 +4,29 @@
 ModulesKrasnitskaAddInt():
 
     int ModulesKrasnitska(int i1, int i2);
+int main() {
+    double gasUsage = getGasUsage();
+    double gasPrice = calculateGasPrice(gasUsage);
+    printGasPrice(gasUsage, gasPrice);
+    return 0;
+}
+#pragma once
+
+#include <string>
+#include <utility>
+
+using namespace std;
+
+pair<string, string> convert_size(pair<string, string> sizes);
+
+#include <iostream>
+
+int main() {
+    unsigned int n;
+    std::cout << "Введіть невід'ємне ціле число N: ";
+    std::cin >> n;
+    
+    int count = 0;
+    int bit = n & 1;
 
 #endif // MODULESKRASNITSKA_H_INCLUDED
