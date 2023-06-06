6d1202db5818a661c34f3e7bec06b16cd0a8ce49
lissavzx <114089485+lissavzx@users.noreply.github.com>
2023-06-06 16:43:38 +0300

    Create ModulesKrasnitska.h

---
+++ lab12/prj/ModulesKrasnitska.h
@@ -0,0 +1,55 @@
+#ifndef MODULESKRASNITSKA_H_INCLUDED
+#define MODULESKRASNITSKA_H_INCLUDED
+
+ModulesKrasnitskaAddInt():
+
+    int ModulesKrasnitska(int i1, int i2);
+int main() {
+    double gasUsage = getGasUsage();
+    double gasPrice = calculateGasPrice(gasUsage);
+    printGasPrice(gasUsage, gasPrice);
+    return 0;
+}
+#pragma once
+
+#include <string>
+#include <utility>
+
+using namespace std;
+
+pair<string, string> convert_size(pair<string, string> sizes);
+
+#include <iostream>
+
+int main() {
+    unsigned int n;
+    std::cout << "Введіть невід'ємне ціле число N: ";
+    std::cin >> n;
+    
+    int count = 0;
+    int bit = n & 1;
+
+    #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+int main() {
+    std::ofstream outfile("input.txt", std::ios_base::app); 
+#include <iostream>
+#include <fstream>
+#include "Krasnitska.h"
+
+using namespace std;
+
+int main() {
+    double x, y, z;
+    int v;
+
+#endif // MODULESKRASNITSKA_H_INCLUDED
